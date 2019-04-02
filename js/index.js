bcabc8247a9f4ca7ac7bd64ddc661302ce20872b
gymdw140472 <dseo9@my.bcit.ca>
2019-04-02 14:25:30 -0700

    Made scrolling effect when clicked on mouse

--- js/index.js
+++ js/index.js
@@ -13,12 +13,12 @@ $(document).ready(function () {
         }
     });
 
-    // $('a.scroll-link').click(function(e){
-	// 	e.preventDefault();
-	// 	$id = $(this).attr('href');
-	// 	$('body,html').animate({
-	// 		scrollTop: $($id).offset.top -60
-	// 	}, 750);
-	// });
+    $('a.scroll-link').click(function(e){
+		e.preventDefault();
+		$id = $(this).attr('href');
+		$('body,html').animate({
+			scrollTop: $($id).offset().top -50
+		}, 750);
+	});
   });
   
