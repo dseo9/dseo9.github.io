8fdf8589ca3790f630afc4ddc1f95463dd4ed22e
gymdw140472 <dseo9@my.bcit.ca>
2019-04-02 14:17:23 -0700

    Added mouse animation, made navigation float right

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
     var scroll_start = 0;
-    var startchange = $('.header');
+    var startchange = $('#resume');
     var offset = startchange.offset();
 
     $(document).scroll(function() { 
-        console.log("scrolling");
         scroll_start = $(this).scrollTop();
 
         if(scroll_start > offset.top) {
-            console.log("change to gray");
-            $('.navigation').css({"background-color": "#ffffff", "transition": "background-color 900ms linear"});
+            $('.navigation').css({"background-color": "#ffffff", "transition": "background-color 200ms linear"});
         } else {
-            console.log("stay transparent");
             $('.navigation').css({"background-color": "transparent", "transition": "background-color 200ms linear"});
         }
     });
+
+    // $('a.scroll-link').click(function(e){
+	// 	e.preventDefault();
+	// 	$id = $(this).attr('href');
+	// 	$('body,html').animate({
+	// 		scrollTop: $($id).offset.top -60
+	// 	}, 750);
+	// });
   });
   
