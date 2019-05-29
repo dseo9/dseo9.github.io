d54f1be17bc9102cd5a49d871b4267b0fc47f084
gymdw140472 <dseo9@my.bcit.ca>
2019-05-29 11:18:31 -0700

    change font

--- js/index.js
+++ js/index.js
@@ -8,13 +8,11 @@ $(document).ready(function () {
         scroll_start = $(this).scrollTop();
 
         if(scroll_start > offset.top) {
-            console.log("change to white navbar ");
             $('.navigation').css({"background-color": "#ffffff", "transition": "background-color 300ms linear"});
             $('.home').css({"color": "black"});
             $('.projects').css({"color": "black"});
             $('.contact').css({"color": "black"});
         } else {
-          console.log("change to transparent");
             $('.navigation').css({"background-color": "transparent", "transition": "background-color 300ms linear"});
             $('.home').css({"color": "#ffffff"});
             $('.projects').css({"color": "#ffffff"});
